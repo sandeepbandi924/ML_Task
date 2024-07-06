3d1f541d81dc27537d38879f67e7ab4e4728ed7c
Sandeep Bandi <bandisandeep1374@gmail.com>
2024-07-06 12:06:28 +0530

    task

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 import joblib
-
-def generate_feed(user_id, model, features_df):
-    user_posts = features_df[features_df['User_ID'] == user_id].copy()
-    user_X = user_posts.drop(['User_ID', 'Post_ID', 'Relevant'], axis=1)
-    user_posts['Relevance_Score'] = model.predict_proba(user_X)[:, 1]
-    return user_posts.sort_values(by='Relevance_Score', ascending=False)
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Welcome to the ML API!'
+    return render_template('index.html')
 
 @app.route('/generate_feed', methods=['POST'])
 def generate_feed_api():
@@ -20,6 +14,14 @@ def generate_feed_api():
     loaded_features_df = joblib.load('features_df.joblib')
     sample_feed = generate_feed(user_id, loaded_model, loaded_features_df)
     return jsonify(sample_feed.to_dict('records'))
+
+
+def generate_feed(user_id, model, features_df):
+    user_posts = features_df[features_df['User_ID'] == user_id].copy()
+    user_X = user_posts.drop(['User_ID', 'Post_ID', 'Relevant'], axis=1)
+    user_posts['Relevance_Score'] = model.predict_proba(user_X)[:, 1]
+    return user_posts.sort_values(by='Relevance_Score', ascending=False)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
